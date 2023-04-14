68ac797defc654b7fc437ea61d96f168965c3910
Chris-tiee <czb00@mail.aub.edu>
2023-04-14 17:56:57 +0300

    deleted:    src/pages/CarAPI.json 	modified:   src/pages/CarListing.js 	modified:   src/pages/CompareCars.js

--- src/pages/CarListing.js
+++ src/pages/CarListing.js
@@ -1,23 +1,22 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 
 const CarListing = () => {
-    const carName = "test1";
-    // // https://royalmotors.azurewebsites.net/car/${carName}
-    // // ./CarAPI.json
-    fetch(`https://royalmotors.azurewebsites.net/car/${carName}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+    const carName = "test3";
+    const [data, setData] = useState([]);
 
-
-
+    useEffect(() => {
+        fetch(`https://royalmotors.azurewebsites.net/car/${carName}`)
+        .then((response) => response.json())
+        .then((data) => setData(data));
+    }, []);
 
   return (
     <div>
         <div className="mainCarSection">
             <div className="big-car-info">
-                <h2>GAC GS5 270T</h2>
-                <h2 className="buffer">buffer</h2>
+                <h2>{data.name}</h2>
+                <h2 className="buffer">buffer</h2> 
                 <button>TEST DRIVE</button>
             </div>
         <img className="mainImg" src="Car pictures/noBackground.png" alt="Main" />
@@ -46,14 +45,14 @@ const CarListing = () => {
                     <h1 className="title" id="SPECIFICATIONS">SPECIFICATIONS</h1>
                 </div>
                 <ol>
-                    <li><strong>Vehicle Type:</strong> SUV</li>
-                    <li><strong>Fuel: </strong>PETROL</li>
-                    <li><strong>Fuel tank capacity:</strong> 55L</li>
-                    <li><strong>Color:</strong> Moonlight Grey</li>
+                    <li><strong>Make: </strong>{data.make}</li>
+                    <li><strong>Model: </strong>{data.model}</li>
+                    <li><strong>Fuel tank capacity: </strong> 55L</li>
+                    <li><strong>Color: </strong>{data.color}</li>
                     <li><strong>Transmission Type: </strong>AUTOMATIC</li>
-                    <li><strong>Model Year:</strong> 2022</li>
-                    <li><strong>Mileage: </strong>14000 KM</li>
-                    <li><strong>Price Including VAT:</strong> 16900 USD</li>
+                    <li><strong>Model Year: </strong>{data.year}</li>
+                    <li><strong>Mileage: </strong>{data.mileage}</li>
+                    <li><strong>Price Including VAT: </strong>{data.price}</li>
                 </ol>
             </div>
         </div>
