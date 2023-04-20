e6c0a71919a659e984a10c5fecf8071ff93c9a65
Nadim Dib <ngd04@mail.aub.edu>
2023-04-20 15:15:13 +0300

    modified:   src/pages/HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,11 +7,61 @@ import { Switch, Route ,Router } from 'react-router-dom';
 
 const HomePage = () => {
     const [data_all, setData] = useState([]);
+    const [carNames, setCarNames] = useState([]);
+    const [carMakes, setCarMakes] = useState([]);
+    const [carModels, setCarModels] = useState([]);
+    const [carYears, setCarYears] = useState([]);
+    const [carUsed, setCarUsed] = useState([]);
+    const [carPrices, setCarPrices] = useState([]);
+    const [filter, setFilter] = useState("");
+    const [sort, setSort] =useState([]);
+
     useEffect(() => {
-        fetch('https://royalmotors.azurewebsites.net/car')
+      fetch('https://royalmotors.azurewebsites.net/car')
         .then((response) => response.json())
-        .then((data_all) => setData(data_all));
+        .then((data_all) => {
+          setData(data_all);
+          setCarNames(Array.from(new Set(data_all.map((car) => car.name))));
+          setCarMakes(Array.from(new Set(data_all.map((car) => car.make))));
+          setCarModels(Array.from(new Set(data_all.map((car) => car.model))));
+          setCarYears(Array.from(new Set(data_all.map((car) => car.year))));
+          setCarUsed(Array.from(new Set(data_all.map((car) => car.used))));
+          setCarPrices(Array.from(new Set(data_all.map((car) => car.price))));
+        });
     }, []);
+
+    const carUsedString = carUsed.map(value => value ? "Used" : "New");
+
+    const handleFilterChange = (event) => {
+      const selectedFilter = event.target.value;
+      setFilter(selectedFilter);
+      if (selectedFilter === ""){
+        setSort([]);
+      }
+      else if (selectedFilter === 'name') {
+        setSort(carNames);
+      } 
+      else if (selectedFilter === 'make') {
+        setSort(carMakes);
+      }
+      else if (selectedFilter === 'model') {
+        setSort(carModels);
+      }
+      else if (selectedFilter === 'year') {
+        setSort(carYears);
+      }
+      else if (selectedFilter === 'used') {
+        setSort(carUsedString);
+      }
+      else if (selectedFilter === 'price') {
+        setSort(carPrices);
+      }
+    };
+    
+    const handleSortChange = (event) => {
+      console.log(`Selected ${filter} filter and ${event.target.value} sort`);
+    };
+
   return (
     <div>
         <div className="kitkat"></div>
@@ -31,10 +81,17 @@ const HomePage = () => {
     {/* <!-- Search bar, Filter and  Sort --> */}
     <div className="SFS">
         <div className="search-container">
-            <div className="butt1">
-                <button className="filter">
-                    <div>Filter</div>
-                </button>
+            {/* Filter dropdown */}
+          <div className="butt1">
+          <select className="filter" value={filter} onChange={handleFilterChange}>
+              <option value="">No Filter</option>
+              <option value="name">Name</option>
+              <option value="make">Make</option>
+              <option value="model">Model</option>
+              <option value="year">Year</option>
+              <option value="used">Used</option>
+              <option value="price">Price</option>
+              </select>
             </div>
 
             <form action="/search">
@@ -43,9 +100,12 @@ const HomePage = () => {
             </form>
 
             <div className="butt2">
-                <button className="sort">
-                    <div>Sort by</div>
-                </button>
+              <select className="sort" value="" onChange={handleSortChange}>
+                <option value="">Select {filter} to sort by</option>
+                  {sort.map((value) => (
+                  <option key={value} value={value}>{value}</option>
+                  ))}  
+              </select>
             </div>
         </div>
     </div>
