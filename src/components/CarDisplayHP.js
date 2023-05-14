9c41e158b29dcaa39a2e7fdcb82177bf65763aea
NadimDeeb <ngd04@aub.edu.lb>
2023-05-14 23:55:53 +0300

    modified:   src/components/CarDisplayHP.js

--- src/components/CarDisplayHP.js
+++ src/components/CarDisplayHP.js
@@ -5,8 +5,8 @@ const CarDisplayHP = (props) => {
   const mainImage = props.image_id_list.split(",")[1] || props.image_id_list.split(",")[2] || props.image_id_list.split(",")[3] || props.image_id_list.split(",")[4] || props.image_id_list.split(",")[5] || props.image_id_list.split(",")[6] || props.image_id_list.split(",")[7] || props.image_id_list.split(",")[8] || props.image_id_list.split(",")[9] || props.image_id_list.split(",")[10]; 
   return (
     <div className="image-box">
-      <img id="GridPic" src={props.image_id_list ? "https://royalmotors.azurewebsites.net/image/" + mainImage : ""} alt="" />
       <Link to={`/${props.name}`} style={{ cursor: 'pointer',textDecoration: 'none' }}>
+      <img id="GridPic" src={props.image_id_list ? "https://royalmotors.azurewebsites.net/image/" + mainImage : ""} alt="" />
         <div className="color-box">
           <p className="CarName">{props.name}</p>
           <p className="subText">Year: {props.year}</p>
