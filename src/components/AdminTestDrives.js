4015b15269030d3a82c28518643e9eefaf4a6cf8
NadimDeeb <ngd04@aub.edu.lb>
2023-05-16 00:47:06 +0300

    modified:   src/components/AdminTestDrives.js 	modified:   src/components/UserTestDrives.js

--- src/components/AdminTestDrives.js
+++ src/components/AdminTestDrives.js
@@ -1,100 +1,180 @@
-import React from 'react'
+import React from 'react';
 import axios from 'axios';
-import "./AdminTestDrives.css"
+import './AdminTestDrives.css';
 import { useState, useEffect } from 'react';
 import TableRowTestDrives from './TableRowTestDrives';
 import { getUserToken } from '../pages/localStorage';
 
 const AdminTestDrives = () => {
   const [testDrives, setTestDrives] = useState([]);
-  const [testCars, setTestCars] = useState([]);
-  const [testName, setTestFirstName] = useState([]);
-  const [testLastName, setTestLastName] = useState([]);
-  const [testTime, setTestTime] = useState([]);
-
-  //make sorted by time test drive array
-
-  const [searchQuery, setSearchQuery] = useState(''); //user input in search bar
+  const [filteredTestDrives, setFilteredTestDrives] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedSortOption, setSelectedSortOption] = useState('');
+  const [sortSwitch, setSortSwitch] = useState(false);
 
   useEffect(() => {
     fetch('https://royalmotors.azurewebsites.net/testdrive', {
-      headers:{
-        'Authorization': `Bearer ${getUserToken()}`,
-      }
+      headers: {
+        Authorization: `Bearer ${getUserToken()}`,
+      },
     })
       .then((response) => response.json())
       .then((data_all) => {
-        setTestDrives(data_all); //stores all the test drive info
-        setTestCars(data_all.map((obj) => obj.car.name)); //stores all the car names
-        setTestFirstName(data_all.map((obj) => obj.account.firstname)); //stores all the first names
-        setTestLastName(data_all.map((obj) => obj.account.lastname)); //stores all the last names
-        setTestTime(data_all.map((obj) => obj.time)); //stores all the times
+        setTestDrives(data_all);
       });
-  }, []);
+  }, [sortSwitch]);
 
-  //add function to sort by time when backend is ready
+  useEffect(() => {
+    if (selectedSortOption !== '') {
+      let sortedTestDrives = [...testDrives];
+
+      switch (selectedSortOption) {
+        case 'alphab':
+          sortedTestDrives.sort((a, b) =>
+            a.car.name.localeCompare(b.car.name)
+          );
+          break;
+        case 'DT':
+          sortedTestDrives.sort((a, b) => a.time - b.time);
+          break;
+        case 'Name':
+          sortedTestDrives.sort((a, b) =>
+            `${a.account.firstname} ${a.account.lastname}`.localeCompare(
+              `${b.account.firstname} ${b.account.lastname}`
+            )
+          );
+          break;
+        default:
+          break;
+      }
+
+      setTestDrives(sortedTestDrives);
+    }
+  }, [selectedSortOption, testDrives]);
+
+  function handleSortChange(event) {
+    setSelectedSortOption(event.target.value);
+  }
+
+  function handleSortReset(event) {
+    if (sortSwitch === false) {
+      setSortSwitch(true);
+    } else {
+      setSortSwitch(false);
+    }
+  }
 
   function handleSearchInputChange(event) {
     setSearchQuery(event.target.value);
-    // handleSearchDisplayedCars(event); I will add the function later
-  };
+    handleSearchDisplayedCars();
+  }
+
+  function handleSearchDisplayedCars() {
+    const filteredDrives = testDrives.filter((drive) =>
+      drive.car.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredTestDrives(filteredDrives);
+  }
 
   const checkIfTimePassed = (unixTime) => {
-    const currentTime = new Date().getTime() / 1000; // Get current Unix time
+    const currentTime = new Date().getTime() / 1000;
 
     if (unixTime < currentTime) {
-      return true; // Time has passed
+      return true;
     } else {
-      return false; // Time has not passed
+      return false;
     }
   };
 
+  useEffect(() => {
+    handleSearchDisplayedCars();
+  }, [searchQuery]);
+
   return (
     <div>
-      {/* Section break */}
-    <div className="kitkat"></div>
-    {/* Search Bar */}
-    <div className="SFS">
-      <div className="search-container">
-        <input type="text" placeholder="Search car name..." value={searchQuery} onChange={handleSearchInputChange}/>
+      <div className="kitkat"></div>
+      <div className="SFS">
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search car name..."
+            value={searchQuery}
+            onChange={handleSearchInputChange}
+          />
 
-        {/* Sort Button */}
-        <div className="butt3">
-          <select className="sort"> 
-            <option value="">No Sort</option>
-            <option value="alphab">Alphabetically</option>
-            <option value="DT">Date/Time</option>  
-          </select>
+          <div className="butt3">
+            <select
+              className="sort"
+              value={selectedSortOption}
+              onChange={handleSortChange}
+            >
+              <option value="" onClick={handleSortReset}>
+                No Sort
+              </option>
+              <option value="alphab">Car Alphabetically</option>
+              <option value="DT">Date/Time</option>
+              <option value="Name">Username Alphabetically</option>
+            </select>
+          </div>
         </div>
       </div>
-    </div>
     {/* Table */}
     <table>
+      <tbody>
         <tr>
-          <td> </td> {/* car name */}
+          <td></td>
           <td id="top">Username</td>
           <td id="top">Date/Time</td>
         </tr>
-        {/* Rows */}
-        {testDrives.length > 0 ? (
-          testDrives.map((obj, i) => {
-            if (!checkIfTimePassed(obj.time)) {
-              return (
-                <TableRowTestDrives
-                  key={i}
-                  name={obj.car.name}
-                  firstname={obj.account.firstname}
-                  lastname={obj.account.lastname}
-                  time={obj.time}
-                  id={obj.id}
-                />
-              );
-            }
-            return null;
-          })
-        ) : (
-          <p>Loading data...</p>
-        )}  
+        {searchQuery !== '' ? (
+  filteredTestDrives.length > 0 ? (
+    filteredTestDrives.map((obj, i) => {
+      if (!checkIfTimePassed(obj.time)) {
+        return (
+          <TableRowTestDrives
+            key={i}
+            name={obj.car.name}
+            firstname={obj.account.firstname}
+            lastname={obj.account.lastname}
+            time={obj.time}
+            id={obj.id}
+          />
+        );
+      }
+      return null;
+    })
+  ) : (
+    <tr>
+      <td colSpan="3">
+        <p>No matching test drives found</p>
+      </td>
+    </tr>
+)) : (
+  testDrives.length > 0 ? (
+    testDrives.map((obj, i) => {
+      if (!checkIfTimePassed(obj.time)) {
+        return (
+          <TableRowTestDrives
+            key={i}
+            name={obj.car.name}
+            firstname={obj.account.firstname}
+            lastname={obj.account.lastname}
+            time={obj.time}
+            id={obj.id}
+          />
+        );
+      }
+      return null;
+    })
+  ) : (
+    <tr>
+      <td colSpan="3">
+        <p>No test drives scheduled</p>
+      </td>
+    </tr>
+  )
+)}  
+      </tbody>
     </table>
     </div>
   )
