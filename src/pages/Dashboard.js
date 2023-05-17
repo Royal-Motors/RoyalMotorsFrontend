febd783ee6a2f9782b91c056c0065a6c8d9788ad
Fawzia <fhz02@mail.aub.edu>
2023-05-17 06:11:15 +0300

    removed dashboard graph till solved (giving bug)

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -6,16 +6,5 @@ import { Paper } from '@mui/material';
 import ReactFrappeChart from "react-frappe-charts";
 
 export default function CarMonthlyChart(props) {
-  return (
-    <ReactFrappeChart
-      type="bar"
-      colors={["#fff"]}
-      axisOptions={{ xAxisMode: "tick", yAxisMode: "tick", xIsSeries: 1 }}
-      height={250}
-      data={{
-        labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-        datasets: [{ values: [18, 40, 30, 35, 8, 52, 17, 4] }],
-      }}
-    />
-  );
+
     }
