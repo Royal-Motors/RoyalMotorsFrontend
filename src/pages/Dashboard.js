65d9e541bd856ec78a8ad835a91d889b0d0c3402
Fawzia <fhz02@mail.aub.edu>
2023-05-17 05:22:34 +0300

    fix forgot pass popup bug

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,7 +3,19 @@ import { Line } from 'react-chartjs-2';
 import { useTheme } from '@mui/material/styles';
 import { Paper } from '@mui/material';
 
-const CarMonthlyChart = () => {
-    
-}
-  export default CarMonthlyChart;
+import ReactFrappeChart from "react-frappe-charts";
+
+export default function CarMonthlyChart(props) {
+  return (
+    <ReactFrappeChart
+      type="bar"
+      colors={["#fff"]}
+      axisOptions={{ xAxisMode: "tick", yAxisMode: "tick", xIsSeries: 1 }}
+      height={250}
+      data={{
+        labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        datasets: [{ values: [18, 40, 30, 35, 8, 52, 17, 4] }],
+      }}
+    />
+  );
+    }
--- src/pages/UserCredentialsDialog/UserCredentialsDialogIn.js
+++ src/pages/UserCredentialsDialog/UserCredentialsDialogIn.js
@@ -1,10 +1,14 @@
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
 import React, { useState } from "react";
 import "./UserCredentialsDialog.css";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { getUserToken, getUserEmail } from '../localStorage';
 // Component that presents a dialog to collect credentials from the user
 export default function UserCredentialsDialogIn({
 open,
@@ -16,7 +20,54 @@ submitText,
 
     let [email, setEmail] = useState("");
     let [password, setPassword] = useState("");
+    let [newPassword, setNewPassword] = useState("");
+    let [openForgot, setOpenForgot] = useState(false);
+    let [code, setCode] = useState("");
+    let [token, setToken] = useState(getUserToken());
 
+    const handleForgotPassword = () =>{
+setOpenForgot(true);
+    }
+    const handleEmail = (event) => {
+        setEmail(event.target.value);
+    }
+    const handleNewPassword = (event) =>{
+        setNewPassword(event.target.value);
+    }
+    const handleClosePass = () => {
+        setOpenForgot(false);
+    }
+
+    function handleNext(){
+        return fetch(`https://royalmotors.azurewebsites.net/account/reset`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+                  },
+            body: JSON.stringify({
+            code : code,
+            password: newPassword,
+            email: email,
+            }),
+          })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(response.statusText);
+            }
+            console.log(response);
+            handleClosePass();
+            return response.json();
+          })
+            .then((data) => {
+              console.log(data);
+            })
+            .catch((error) => console.error(error));
+    }
+
+    const handleCode = (event) =>{
+        setCode(event.target.value);
+    }
     const theme = createTheme({
     palette: {
         primary: {
@@ -25,7 +76,10 @@ submitText,
     },
     });
 
+    
+
     return (
+        <>
         <Dialog open={open} onClose={onClose} maxWidth="xs" fullWidth>
         <div className="dialog-container">
 
@@ -48,6 +102,9 @@ submitText,
                 value={password} 
                 onChange={({ target: { value } }) => setPassword(value)}
             />
+            <div>
+                <p onClick={handleForgotPassword}>Forgot password?</p>
+            </div>
             </div>
             <ThemeProvider theme={theme}>
             <Button
@@ -59,5 +116,46 @@ submitText,
             </ThemeProvider>
             </div>
         </Dialog>
+
+<Dialog open={openForgot} onClose={handleClosePass}>
+        <DialogTitle>Change Password</DialogTitle>
+        <DialogContent>
+        <TextField
+            autoFocus
+            margin="dense"
+            id="email"
+            label="Email"
+            type="email"
+            fullWidth
+            variant="standard"
+            onChange={handleEmail}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="new"
+            label="New password"
+            type="password"
+            fullWidth
+            variant="standard"
+            onChange={handleNewPassword}
+          />
+            <TextField
+            autoFocus
+            margin="dense"
+            id="code"
+            label="Code"
+            type="code"
+            fullWidth
+            variant="standard"
+            onChange={handleCode}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClosePass}>Cancel</Button>
+          <Button onClick={handleNext}>Enter</Button>
+        </DialogActions>
+      </Dialog>
+      </>
     );
 }
